02b3b6abb488ae4f070b1ad31e7951bcf283fbff
shivamsahay0 <60652255+shivamsahay0@users.noreply.github.com>
2020-03-02 20:15:56 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,23 @@
+public class Student implements Issue{
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void issue() {
+        System.out.println(this.id+ " "+this.name + " has issued a book and cannot issue further" );
+
+    }
+}
